bd941dbd6a08c3d5f98c58f813e5935bdf6222c5
nathaniasalsabila28 <63964310+nathaniasalsabila28@users.noreply.github.com>
2020-04-21 07:33:01 +0700

    Add files via upload

---
+++ aset/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<script src="http://localhost/siperpus/aset/jquery.js"></script>
+	<script src="http://localhost/siperpus/aset/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
